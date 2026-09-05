e342720101ab0cac9e606f64a1ff8acc6c7f2a65
dany <dany@gmail.com>
2026-09-05 01:46:22 +0100

    Add cart tests and remaining checkout validation edge cases

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -4,9 +4,16 @@ from pages.base_page import BasePage
 class CartPage(BasePage):
     CHECKOUT_BUTTON = (By.ID, "checkout")
     CART_ITEMS = (By.CLASS_NAME, "cart_item")
+    CONTINUE_SHOPPING_BUTTON = (By.ID, "continue-shopping")
 
     def get_item_count(self):
         return len(self.driver.find_elements(*self.CART_ITEMS))
 
     def checkout(self):
         self.click(self.CHECKOUT_BUTTON)
+
+    def remove_item(self, product_slug):
+        self.click((By.ID, f"remove-{product_slug}"))
+
+    def continue_shopping(self):
+        self.click(self.CONTINUE_SHOPPING_BUTTON)
--- tests/test_checkout.py
+++ tests/test_checkout.py
@@ -4,38 +4,26 @@ from pages.inventory_page import InventoryPage
 from pages.cart_page import CartPage
 from pages.checkout_page import CheckoutPage
 from utils.config_reader import CONFIG
-
-@pytest.mark.smoke
-def test_complete_purchase_flow(driver):
-    # Log in
+@pytest.mark.regression
+def test_checkout_requires_last_name(driver):
     login_page = LoginPage(driver)
     login_page.load()
     creds = CONFIG["users"]["standard_user"]
     login_page.login(creds["username"], creds["password"])
 
-    # Add items to cart
     inventory_page = InventoryPage(driver)
     inventory_page.add_to_cart("sauce-labs-backpack")
-    inventory_page.add_to_cart("sauce-labs-bike-light")
-    assert inventory_page.get_cart_count() == 2
-
-    # Go to cart
     inventory_page.go_to_cart()
+
     cart_page = CartPage(driver)
-    assert cart_page.get_item_count() == 2
-
-    # Checkout
     cart_page.checkout()
+
     checkout_page = CheckoutPage(driver)
-    checkout_page.fill_info("Dany", "Test", "12345")
-    checkout_page.finish_order()
-
-    # Confirm order
-    assert checkout_page.get_confirmation_text() == "Thank you for your order!"
-
+    checkout_page.fill_info("Dany", "", "12345")
+    assert "last name is required" in checkout_page.get_error_text().lower()
 
 @pytest.mark.regression
-def test_checkout_requires_first_name(driver):
+def test_checkout_requires_postal_code(driver):
     login_page = LoginPage(driver)
     login_page.load()
     creds = CONFIG["users"]["standard_user"]
@@ -49,6 +37,23 @@ def test_checkout_requires_first_name(driver):
     cart_page.checkout()
 
     checkout_page = CheckoutPage(driver)
-    checkout_page.fill_info("", "Test", "12345")  # missing first name
+    checkout_page.fill_info("Dany", "Test", "")
+    assert "postal code is required" in checkout_page.get_error_text().lower()
 
+@pytest.mark.regression
+def test_checkout_all_fields_empty(driver):
+    login_page = LoginPage(driver)
+    login_page.load()
+    creds = CONFIG["users"]["standard_user"]
+    login_page.login(creds["username"], creds["password"])
+
+    inventory_page = InventoryPage(driver)
+    inventory_page.add_to_cart("sauce-labs-backpack")
+    inventory_page.go_to_cart()
+
+    cart_page = CartPage(driver)
+    cart_page.checkout()
+
+    checkout_page = CheckoutPage(driver)
+    checkout_page.fill_info("", "", "")
     assert "first name is required" in checkout_page.get_error_text().lower()
